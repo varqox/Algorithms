73e6cdc9e3aac7c4b839177ae69f07a21e204698
Krzysztof Małysa <krzyk240@gmail.com>
2014-11-03 23:16:42 +0100

    Some improvements

--- test/trie.cpp
+++ test/trie.cpp
@@ -5,24 +5,28 @@
 
 namespace XXX {
 
-struct none {};
+struct None {};
 
-template<class T = none>
-class Trie {
-private:
+template<class BaseNode>
+class BaseTrie {
+protected:
 	typedef unsigned char uchar;
-	class Node {
+
+	class Node : public BaseNode {
 	private:
 		Node(const Node&);
 		Node& operator=(const Node&);
 
 	public:
 		uchar key_;
-		Node* p_; // parent
+		bool ins_; // is this word inserted
 		class Sons {
 		private:
 			short size_, real_size_;
 			Node** t_;
+
+			Sons(const Sons&);
+			Sons& operator=(const Sons&);
 
 		public:
 			Sons(): size_(0), real_size_(0), t_(NULL) {}
@@ -32,60 +36,27 @@ private:
 			void insert(Node* p);
 			void erase(uchar c);
 		} sons_;
-		T* val_; // value
 
-		Node(uchar key, Node* p = NULL, T* val = NULL) : key_(key), p_(p), sons_(), val_(val) {}
+		Node(uchar key) : key_(key), ins_(false), sons_() {}
 
-		~Node() {
-			if (val_)
-				delete val_;
-		}
+		~Node() {}
 	};
 
 	Node* root;
 
-	Trie(const Trie&); // Have to write
-	Trie& operator=(const Trie&); // Have to write
+	BaseTrie(const BaseTrie&); // Have to write
+	BaseTrie& operator=(const BaseTrie&); // Have to write
 
 public:
-	Trie(): root(new Node('\0')) {}
+	BaseTrie(): root(new Node('\0')) {}
+	virtual ~BaseTrie() { delete root; }
 
-	~Trie() { delete root; }
-
-	class iterator {
-	protected:
-		Node* p_;
-		iterator(Node* x) : p_(x) {}
-
-	public:
-		iterator() : p_(NULL) {}
-
-		iterator(const iterator& it): p_(it.p_) {}
-
-		iterator& operator=(const iterator& it) {
-			p_ = it.p_;
-			return *this;
-		}
-
-		bool operator==(const iterator& it) { return p_ == it.p_; }
-
-		bool operator!=(const iterator& it) { return p_ != it.p_; }
-
-		T& operator*() { return *p_->value; }
-
-		T* operator->() { return p_->value; }
-
-		friend class Trie;
-	};
-
-	void insert(const std::string str, T* val = new T);
-	iterator find(const std::string str);
-
-	iterator end() const { return NULL; }
+	Node* insert(const std::string str);
+	Node* find(const std::string str);
 };
 
-template<class T>
-Trie<T>::Node::Sons::~Sons() {
+template<class BaseNode>
+BaseTrie<BaseNode>::Node::Sons::~Sons() {
 	if (t_) {
 		for (short i = 0; i < size_; ++i)
 			delete t_[i];
@@ -93,8 +64,8 @@ Trie<T>::Node::Sons::~Sons() {
 	}
 }
 
-template<class T>
-typename Trie<T>::Node* Trie<T>::Node::Sons::operator[](uchar c) {
+template<class BaseNode>
+typename BaseTrie<BaseNode>::Node* BaseTrie<BaseNode>::Node::Sons::operator[](uchar c) {
 	// Simple searching (for speed up)
 	if (size_ < 5) {
 		for (short i = 0; i < size_; ++i)
@@ -118,8 +89,8 @@ typename Trie<T>::Node* Trie<T>::Node::Sons::operator[](uchar c) {
 	return NULL;
 }
 
-template<class T>
-void Trie<T>::Node::Sons::insert(Node* p) {
+template<class BaseNode>
+void BaseTrie<BaseNode>::Node::Sons::insert(Node* p) {
 	if (t_ == NULL) { // First son
 		t_ = new Node*[1];
 		size_ = real_size_ = 1;
@@ -143,8 +114,8 @@ void Trie<T>::Node::Sons::insert(Node* p) {
 	}
 }
 
-template<class T>
-void Trie<T>::Node::Sons::erase(uchar c) {
+template<class BaseNode>
+void BaseTrie<BaseNode>::Node::Sons::erase(uchar c) {
 	Node *p = operator[](c); // Find node to erase
 	if (p) { // If exists
 		// Find position
@@ -159,26 +130,29 @@ void Trie<T>::Node::Sons::erase(uchar c) {
 	}
 }
 
-template<class T>
-void Trie<T>::insert(const std::string str, T* val) {
+template<class BaseNode>
+typename BaseTrie<BaseNode>::Node* BaseTrie<BaseNode>::insert(const std::string str) {
 	size_t i = 0, len = str.size();
 	Node *p = root, *k;
+	// Go through existing nodes
 	for (; i < len; ++i) {
 		k = p->sons_[str[i]];
 		if (k == NULL)
 			break;
 		p = k;
 	}
+	// Create missing
 	for (; i < len; ++i) {
-		k = new Node(str[i], p);
+		k = new Node(str[i]);
 		p->sons_.insert(k);
 		p = k;
 	}
-	p->val_ = val;
+	p->ins_ = true; // Mark insertion
+	return p;
 }
 
-template<class T>
-typename Trie<T>::iterator Trie<T>::find(const std::string str) {
+template<class BaseNode>
+typename BaseTrie<BaseNode>::Node* BaseTrie<BaseNode>::find(const std::string str) {
 	size_t i = 0, len = str.size();
 	Node *p = root;
 	for (; i < len; ++i) {
@@ -189,28 +163,98 @@ typename Trie<T>::iterator Trie<T>::find(const std::string str) {
 	return p;
 }
 
+template<class T>
+class TrieNode {
+private:
+	TrieNode(const TrieNode&);
+	TrieNode& operator=(const TrieNode&);
+
+public:
+	TrieNode* p_;
+	T* val_;
+
+	TrieNode(): p_(NULL), val_(NULL) {}
+	virtual ~TrieNode() {
+		if (val_)
+			delete val_;
+	}
+};
+
+template<class T = None>
+class Trie : protected BaseTrie< TrieNode<T> > {
+public:
+	Trie() {}
+	virtual ~Trie() {}
+
+	class iterator {
+	protected:
+		typename BaseTrie< TrieNode<T> >::Node* p_;
+		iterator(typename BaseTrie< TrieNode<T> >::Node* x) : p_(x) {}
+
+	public:
+		iterator() : p_(NULL) {}
+		iterator(const iterator& it): p_(it.p_) {}
+		iterator& operator=(const iterator& it) {
+			p_ = it.p_;
+			return *this;
+		}
+		bool operator==(const iterator& it) { return p_ == it.p_; }
+		bool operator!=(const iterator& it) { return p_ != it.p_; }
+		T& operator*() { return *p_->val_; }
+		T* operator->() { return p_->val_; }
+
+		friend class Trie;
+	};
+
+	iterator insert(const std::string str, T* val = NULL) {
+		typename BaseTrie< TrieNode<T> >::Node* p = BaseTrie< TrieNode<T> >::insert(str);
+		p->val_ = val;
+		return p;
+	}
+	iterator insert(const std::string str, T val) { return insert(str, new T(val)); }
+	iterator find(const std::string str) { return iterator(BaseTrie< TrieNode<T> >::find(str)); }
+	iterator end() const { return NULL; }
+};
+
+template<>
+class Trie<None> : protected BaseTrie<None> {
+private:
+	Trie(const Trie&); // Have to write
+	Trie& operator=(const Trie&); // Have to write
+
+public:
+	Trie() {}
+	virtual ~Trie() {}
+
+	void insert(const std::string str) { BaseTrie::insert(str); }
+
+	bool find(const std::string str) {
+		Node* p = BaseTrie::find(str);
+		return (p == NULL ? false : p->ins_);
+	}
+};
+
 } // namespace XXX
 
 using std::string;
 
 const int MAX_LEN = 20, N = 1000000;
 
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main() {
-	/*XXX::none* p;
-	p = new XXX::none;
-	printf("%i\n", sizeof(*	p));
-	printf("%p\n", p);
-	return 0;*/
 	srand(12984912);
 	// Trie::CompressedTrie<XXX::none> trie;
 	XXX::Trie<> trie;
-	/*trie.insert("abc");
-	trie.insert("ab");
-	printf("%i\n", trie.end() != trie.find("abc"));
-	printf("%i\n", trie.end() != trie.find("ab"));
-	printf("%i\n", trie.end() != trie.find("abb"));
-	return 0;*/
+	/*XXX::Trie<char> k;
+	cout << *k.insert("123", '$') << endl;
+	cout << *k.insert("12", ';') << endl;
+	cout << *k.find("123") << endl;
+	cout << *k.find("12") << endl;*/
 	for (size_t i = 0; i < N; ++i) {
 		string s;
 		for (size_t j = 1 + rand() % MAX_LEN; j > 0; --j)
